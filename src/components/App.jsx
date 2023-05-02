369d2a4cebef5773cb3529d21bc81d449ae49cb3
sohaildotcss <sohailqwerty7@gmail.com>
2023-05-02 19:57:17 +0530

    First stage of Keeper-app-project 🚀

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+// Include all components here //
+
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+}
+export default App;
